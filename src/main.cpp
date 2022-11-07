abd7a0a828bf4e40dcff80c20ed70030e010112e
PoshPoro1 <21mr2420@barton.ac.uk>
2022-11-07 17:31:11 +0000

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "imgui.h"
-#include "backends/imgui_impl_glfw.h"
-#include "backends/imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include "imgui_internal.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
